89f9a0eb3f5c43859c4e14f695cf1cf9b36391a7
Andrea <44727620+Andry96@users.noreply.github.com>
2019-12-27 17:19:04 +0100

    Added TODO for improvement

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -3,17 +3,24 @@ import React from "react";
 // componente <Tag />
 const tagStyle = {
   display: "inline-block",
-  backgroundColor: "yellow",
+  //backgroundColor: "withe",
   fontSize: "0.9em",
   margin: "5px",
   border: "1px solid lightblue",
   padding: "2px",
   cursor: "pointer"
 };
-function Tag({ onDeleteTag, value }) {
+
+/*TODO: aggiungere la possibilità di dare un colore al tag inserendolo dopo il nome del tag con la sintassi {<colore>}
+ * modificare il parser all'interno di InputTag per identificare l'eventuale colore e passarlo come prop al componente Tag
+ */
+function Tag({ onDeleteTag, value, color }) {
   var tag = (
-    <div class="tag-item">
-      <span onClick={e => onDeleteTag(e, value)} style={tagStyle}>
+    <div style={tagStyle}>
+      <span
+        style={{ tagStyle, backgroundColor: color }}
+        onClick={e => onDeleteTag(e, value)}
+      >
         &#x2716;{" "}
       </span>
       {value}
